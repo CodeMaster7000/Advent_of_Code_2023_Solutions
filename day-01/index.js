6a39a3160ab4261da398aeeb56bebbdcaf588772
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-12-01 20:43:17 +0000

    Update index.js

--- day-01/index.js
+++ day-01/index.js
@@ -7,7 +7,7 @@ const Part1 = fs.readFileSync('./day-01/input.in', 'utf8')
     return parseInt(`${firstDigit}${lastDigit}`);
   })
   .reduce((acc, curr) => acc + curr, 0);
-const Part2 = fs.readFileSync('./day-01/input.txt', 'utf8')
+const Part2 = fs.readFileSync('./day-01/input.in', 'utf8')
   .split('\n')
   .map(row => {
     const numberMap = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
