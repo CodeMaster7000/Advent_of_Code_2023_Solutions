93a764dbdf59e194acd791ce23d95e442dd845ef
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-12-06 16:17:43 +0000

    Add files via upload

---
+++ day-05/index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.in', 'utf8').trimEnd();
+function Part1(input) {
+  let [seeds, ...maps] = input.split('\n\n');
+  seeds = seeds.match(/\d+/g).map(Number);
+  for (let map of maps) {
+    map = map
+      .split('\n')
+      .slice(1)
+      .map((line) => line.match(/\d+/g).map(Number));
+    for (let i = 0; i < seeds.length; i++) {
+      const seed = seeds[i];
+      for (const [dest, source, len] of map) {
+        if (seed >= source && seed < source + len) {
+          seeds[i] = seeds[i] - source + dest;
+          break;
+        }
+      }
+    }
+  }
+  console.log(Math.min(...seeds));
+}
+Part1(input); // Part 1 solution
+function Part2(input) {
+  let [seeds, ...maps] = input.split('\n\n');
+  seeds = seeds.match(/\d+/g).map(Number);
+  const nextSeeds = [];
+  for (let i = 0; i < seeds.length; i += 2) {
+    nextSeeds.push([seeds[i], seeds[i] + seeds[i + 1] - 1]);
+  }
+  seeds = nextSeeds;
+  for (let map of maps) {
+    map = map
+      .split('\n')
+      .slice(1)
+      .map((line) => line.match(/\d+/g).map(Number));
+    const movedSeeds = [];
+    for (const [dest, source, len] of map) {
+      const unmovedSeeds = [];
+      for (const [start, end] of seeds) {
+        if (start < source + len && end >= source) {
+          movedSeeds.push([
+            Math.max(start, source) - source + dest,
+            Math.min(end, source + len - 1) - source + dest,
+          ]);
+        }
+        if (start < source) {
+          unmovedSeeds.push([start, Math.min(end, source - 1)]);
+        }
+        if (end >= source + len) {
+          unmovedSeeds.push([Math.max(start, source + len), end]);
+        }
+      }
+      seeds = unmovedSeeds;
+    }
+    seeds.push(...movedSeeds);
+  }
+  console.log(Math.min(...seeds.flat()));
+}
+Part2(input); // Part 2 solution
